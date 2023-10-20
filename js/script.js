f10ed4096b5c0c1bab60675884f73c54bfe13c52
Murilo <murilo.ti32@outlook.com>
2023-10-20 16:01:34 -0300

    função de requisição da API e renderização dos dados

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
+const pokemonName = document.querySelector('.pokemon-name')
+
+
 //Função para pegar os dados do POKEMON requisitado 
 const fetchPokemon = async (pokemon) => {
-
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     const data = await APIResponse.json();
     return data;
 }
 
 //Função para RENDERIZAR a imagem do pokemon que foi requisitado
-const renderPokemon = (pokemon) => {
-    
-    const data = fetchPokemon(pokemon)
+const renderPokemon = async (pokemon) => {    
+    const data = await fetchPokemon(pokemon);
+    console.log(data);
+    document.querySelector('.pokemon-name').innerHTML = data.name;
 
-
-    
 }
+
+fetchPokemon('25')
+renderPokemon('25')
